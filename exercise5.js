7faf342fda34624665f98c7e592feb4920952780
DelphicOkami <DelphicOkami@hrafn.xyz>
2022-02-17 09:39:27 +0000

    Add 5

--- exercise5.js
+++ exercise5.js
@@ -5,7 +5,7 @@
  * 1. https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
  * 2. https://www.codewars.com/kata/571edd157e8954bab500032d
  * 3. https://www.codewars.com/kata/571edea4b625edcb51000d8e
- * 4. https://www.codewars.com/kata/571effabb625ed9b0600107a
+ * 4. https://www.codewars.com/kata/571effabb625ed9b0600107ag
  * 5. https://www.codewars.com/kata/571f1eb77e8954a812000837
  * 6. https://www.codewars.com/kata/571f832f07363d295d001ba8
  * 7. https://www.codewars.com/kata/57202aefe8d6c514300001fd
